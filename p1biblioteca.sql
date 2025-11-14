1086fd8e86df19a13a7f29c2a45b4f89c9a40b50
tony-4646 <menesesanthony2006@gmail.com>
2025-11-14 16:40:36 -0500

    Base de datos añadida

---
+++ p1biblioteca.sql
@@ -0,0 +1,172 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 14-11-2025 a las 22:39:50
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `p1biblioteca`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `libros`
+--
+
+CREATE TABLE `libros` (
+  `idlibro` int(11) NOT NULL,
+  `lnombre` varchar(100) DEFAULT NULL,
+  `lautor` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `libros`
+--
+
+INSERT INTO `libros` (`idlibro`, `lnombre`, `lautor`) VALUES
+(1, 'El Gato Negro', 'Edgar Allan Poe'),
+(2, 'Manifesto Socialista', 'Paulo albedo'),
+(3, 'Amar y querer', 'Gabriel Hurtado');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `prestamos`
+--
+
+CREATE TABLE `prestamos` (
+  `idprestamo` int(11) NOT NULL,
+  `idusuario` int(11) DEFAULT NULL,
+  `idlibro` int(11) DEFAULT NULL,
+  `pfechaprestamo` date DEFAULT NULL,
+  `pfechadevolucion` date DEFAULT NULL
+) ;
+
+--
+-- Volcado de datos para la tabla `prestamos`
+--
+
+INSERT INTO `prestamos` (`idprestamo`, `idusuario`, `idlibro`, `pfechaprestamo`, `pfechadevolucion`) VALUES
+(1, 1, 1, '2025-11-14', '2025-11-15'),
+(2, 2, 2, '2025-11-14', NULL),
+(3, 2, 3, '2025-11-14', '2025-11-14'),
+(5, 1, 1, '2025-11-14', '2025-11-14'),
+(6, 2, 3, '2025-11-14', '2025-11-14'),
+(7, 1, 1, '2025-11-14', NULL);
+
+--
+-- Disparadores `prestamos`
+--
+DELIMITER $$
+CREATE TRIGGER `disponibilidad_prestamos` BEFORE INSERT ON `prestamos` FOR EACH ROW BEGIN
+    DECLARE libro_prestado INT;
+
+    SELECT COUNT(*)
+    INTO libro_prestado
+    FROM prestamos
+    WHERE idlibro = NEW.idlibro
+      AND pfechadevolucion IS NULL;
+
+    IF libro_prestado > 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Error: Este libro ya se encuentra actualmente prestado.';
+    END IF;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `idusuario` int(11) NOT NULL,
+  `unombre` varchar(100) DEFAULT NULL,
+  `utelefono` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`idusuario`, `unombre`, `utelefono`) VALUES
+(1, 'Anthony Gabriel', '0959981566'),
+(2, 'Kevin Meneses', '092812312');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `libros`
+--
+ALTER TABLE `libros`
+  ADD PRIMARY KEY (`idlibro`);
+
+--
+-- Indices de la tabla `prestamos`
+--
+ALTER TABLE `prestamos`
+  ADD PRIMARY KEY (`idprestamo`),
+  ADD KEY `idusuario` (`idusuario`),
+  ADD KEY `idlibro` (`idlibro`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`idusuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `libros`
+--
+ALTER TABLE `libros`
+  MODIFY `idlibro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `prestamos`
+--
+ALTER TABLE `prestamos`
+  MODIFY `idprestamo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `idusuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `prestamos`
+--
+ALTER TABLE `prestamos`
+  ADD CONSTRAINT `prestamos_ibfk_1` FOREIGN KEY (`idusuario`) REFERENCES `usuarios` (`idusuario`),
+  ADD CONSTRAINT `prestamos_ibfk_2` FOREIGN KEY (`idlibro`) REFERENCES `libros` (`idlibro`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
